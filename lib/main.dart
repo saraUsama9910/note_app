acdea04660d25df9228077ecc9b96d75a0f8bfbd
saraUsama9910 <sarausama80@gmail.com>
2024-03-08 17:20:33 +0200

    change theme and create note view

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/views/note_view.dart';
 
 void main() {
   runApp(const NoteApp());
 }
 
 class NoteApp extends StatelessWidget {
- const NoteApp({super.key});
+  const NoteApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: const NoteView(),
+    );
   }
 }
