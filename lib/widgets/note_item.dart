bace561cfa55ad2d1694fcab6b6cfc03acc135d0
saraUsama9910 <sarausama80@gmail.com>
2024-03-09 19:28:48 +0200

    create edit note view

--- lib/widgets/note_item.dart
+++ lib/widgets/note_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:note_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
@@ -7,45 +8,52 @@ class NoteItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Container(
-        padding: const EdgeInsets.only(top: 24, bottom: 24),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          color: const Color.fromARGB(255, 255, 212, 83),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            ListTile(
-              title: const Text(
-                'flutter tips',
-                style: TextStyle(color: Colors.black, fontSize: 26),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return const EditNoteView();
+          }));
+        },
+        child: Container(
+          padding: const EdgeInsets.only(top: 24, bottom: 24),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: const Color.fromARGB(255, 255, 212, 83),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              ListTile(
+                title: const Text(
+                  'flutter tips',
+                  style: TextStyle(color: Colors.black, fontSize: 26),
+                ),
+                subtitle: Padding(
+                  padding: const EdgeInsets.only(top: 16),
+                  child: Text(
+                    'My Name is Sara I am A Mobile Developer',
+                    style: TextStyle(
+                        color: Colors.black.withOpacity(.4), fontSize: 20),
+                  ),
+                ),
+                trailing: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    FontAwesomeIcons.trash,
+                    color: Colors.red,
+                    size: 30,
+                  ),
+                ),
               ),
-              subtitle: Padding(
-                padding: const EdgeInsets.only(top: 16),
+              Padding(
+                padding: const EdgeInsets.only(right: 24),
                 child: Text(
-                  'My Name is Sara I am A Mobile Developer',
-                  style: TextStyle(
-                      color: Colors.black.withOpacity(.4), fontSize: 20),
+                  'May 21 2024',
+                  style: TextStyle(color: Colors.black.withOpacity(.4)),
                 ),
               ),
-              trailing: IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  FontAwesomeIcons.trash,
-                  color: Colors.red,
-                  size: 30,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 24),
-              child: Text(
-                'May 21 2024',
-                style: TextStyle(color: Colors.black.withOpacity(.4)),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
