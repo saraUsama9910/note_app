cebc799a5c0781268fb7cb28eed2ceda0d1ca02d
saraUsama9910 <sarausama80@gmail.com>
2024-03-13 21:16:32 +0200

    install modal_progress_hud_nsn: ^0.5.1

--- lib/widgets/add_note_button_sheet.dart
+++ lib/widgets/add_note_button_sheet.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:note_app/widgets/add_note_form.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:note_app/widgets/add_note_form.dart';
+
 class AddNoteButton extends StatelessWidget {
   const AddNoteButton({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16),
+      padding: const EdgeInsets.symmetric(horizontal: 16),
       child: SingleChildScrollView(
-        child: BlocConsumer<SubjectBloc, SubjectState>(
+        child: BlocConsumer<AddNoteCubit, AddNoteState>(
           listener: (context, state) {
             // TODO: implement listener
           },
           builder: (context, state) {
-            return AddNoteForm();
+            return const AddNoteForm();
           },
         ),
       ),
